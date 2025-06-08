77b1c0cd6ba91f17276dd740c55e6f8951cbd9d2
Jeff Tickle <jeff@jefftickle.com>
2025-06-08 18:44:04 -0400

    GPLv3

--- src/bin/login.rs
+++ src/bin/login.rs
@@ -1,3 +1,20 @@
+/* Experiment with opening a pam context as another user (basically like `su`)
+ * Copyright (C) 2025 Jeffrey W. Tickle
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 use pam_client::{Context, Flag};
 use pam_client::conv_cli::Conversation;
 use std::process::Command;
--- src/bin/qtty.rs
+++ src/bin/qtty.rs
@@ -1,3 +1,20 @@
+/* TTY over QUIC - client
+ * Copyright (C) 2025 Jeffrey W. Tickle
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 use std::{error::Error, net::ToSocketAddrs, path::Path, os::fd::AsRawFd};
 use clap::Parser;
 use s2n_quic::{Client, provider::io::tokio::Builder as IoBuilder, client::Connect};
